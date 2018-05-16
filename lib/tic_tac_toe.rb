7fdce0cf1ac34b007729e59cd4802592c668fc2e
LeishaBurford <lburf102@uottawa.ca>
2018-05-16 00:55:51 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -74,7 +74,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def move(board, index, token = "X")
+def move(board, index, token)
   board[index] = token
 end
   
