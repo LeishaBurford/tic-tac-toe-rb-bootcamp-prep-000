5bc33ab97c8ac628bf9bd7ad0cb64aa1c733598c
LeishaBurford <lburf102@uottawa.ca>
2018-05-16 00:55:25 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -65,3 +65,47 @@ end
 def current_player(board)
   return (turn_count(board) % 2 == 0) ? "X" : "O"
 end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def move(board, index, token = "X")
+  board[index] = token
+end
+  
+def turn(board)
+  puts "Please enter 1-9:"
+  index = input_to_index(gets.strip)
+  while !valid_move?(board, index)
+    puts "Please enter 1-9:"
+    index = input_to_index(gets.strip)
+  end
+  move(board, index, "X")
+  display_board(board)
+end
+
+def input_to_index(input)
+  return input.to_i - 1
+end
+
+# code your #valid_move? method here
+def valid_move?(board, index)
+  if position_taken?(board, index) || !index.between?(0, 8)
+    return false
+  end
+  return true
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if board[index] == " "
+    return false
+  else
+    return true
+  end
+end
